3ed8ba69f86dac5f43efbf2a0971cb81ff3da897
Yaroslav <yaroslav.kramarchuk@gmail.com>
2022-10-27 12:19:03 +0200

    1st task updated

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -22,24 +22,28 @@ const galleryContainer = document.querySelector(".gallery");
 
 const galleryElementMarkup = createGalleryMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML("beforeend", galleryElementMarkup);
+
 galleryContainer.addEventListener("click", showImage);
+
 function showImage(e) {
 	e.preventDefault();
 	if (!e.target.classList.contains("gallery__image")) {
 		return;
 	}
+
 	const instance = basicLightbox.create(`
     <img src="${e.target.dataset.source}" width="800" height="600">
 `);
 
 	instance.show();
+
+	document.addEventListener("keydown", onEscPress);
+
+	function onEscPress(e) {
+		if (e.code === "Escape") {
+			instance.close();
+		}
+		document.removeEventListener("keydown", onEscPress);
+		console.log(e.code);
+	}
 }
-
-// console.log(galleryItems);
-// Выполняй это задание в файлах 01-gallery.html и 01-gallery.js. Разбей его на несколько подзадач:
-
-// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
-// Реализация делегирования на div.gallery и получение url большого изображения.
-// Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
-// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
-// Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
